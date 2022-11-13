ad6d4a409c68c9d36f91ca3728de9b6a184184c2
Samuel Pérez <3607487240101@ingenieria.usac.edu.gt>
2022-11-12 23:17:11 -0600

    edit image json

--- src/controllers/processFormDataController.js
+++ src/controllers/processFormDataController.js
@@ -25,6 +25,7 @@ const createProcessFormData = async (req, res) => {
 const createProcessFormImages = async (req, res) => {
     const imagesName = req.body.imagesName.split(',')
     const files = req.files
+    let imagesPrevious = req.body.imagesPrevious
 
     if(files !== undefined){
         for(let i = 0; i < files.length; i++){
@@ -34,8 +35,26 @@ const createProcessFormImages = async (req, res) => {
             })
         }
     }
+    if(imagesPrevious !== undefined){
+        imagesPrevious = imagesPrevious.split(',')
+        console.log('imagesPrevious', imagesPrevious)
+        for(let i = 0; i < imagesPrevious.length; i++){
+            if(imagesPrevious[i] !== "") deleteImage(imagesPrevious[i])
+        }
+    }
     res.json({'message': 'create process form images'});
 }
+
+//Eliminar una imagen
+function deleteImage(urlImage){
+    const nameImage = urlImage.split('/')
+    const pathImage = 'src/public/images/' + nameImage[nameImage.length-1]
+    const fs = require('fs').promises
+    fs.unlink(pathImage)
+    .then(() => console.log('File removed'))
+    .catch(err => console.error('ERROR', err))
+}
+
 
 const getProcessesFormData = async (req, res) => {
     const columns = 'PD.id, fields_properties, id_process, name AS process_name, description'
@@ -47,7 +66,7 @@ const getProcessesFormData = async (req, res) => {
 
 const getProcessFormData = async (req, res) => {
     const id = req.params.idProcessData
-    const columns = 'PD.id, fields_properties, name AS process_name'
+    const columns = 'PD.id, fields_properties, name AS process_name, id_process'
     const response = await pool.query(`SELECT ${columns} FROM sp_process_data PD 
         INNER JOIN sp_process P ON PD.id_process = P.id
         WHERE PD.id = ${id}`);
@@ -61,13 +80,25 @@ const getCountProcessFormData = async (req, res) => {
         GROUP BY PD.id_process, P.name`);
 
     res.json(response.rows);
-    console.log(response.rows)
 }
 
 const updateProcessFormData = async(req, res) => {
     const id = req.params.idProcessData
     const {fieldsProperties} = req.body
     const columns = 'fields_properties=$1'
+
+    fieldsProperties.forEach(step => {
+        step.fieldsProperties.forEach(stepField => {
+            if(stepField.fieldType == "file" && stepField.fieldValue !== ""){
+                if(stepField.fieldValue.substring(0,4) !== "http"){
+                    //stepField.fieldValue = 'http://localhost:3000/images/' + stepField.fieldValue
+                    //stepField.fieldValue = process.env.API_URL + `/images/${stepField.fieldValue}`
+                    stepField.fieldValue = 'https://api-workflow.com/images/' + stepField.fieldValue
+                }
+                
+            }
+        })
+    })
     const data = [JSON.stringify(fieldsProperties)]
     await pool.query('UPDATE sp_process_data SET ' + columns + `WHERE id=${id}`, data)
     res.json({message: 'update process data'})
